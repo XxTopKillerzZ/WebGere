973100900e7955967e01410121deb1d68ae4be3b
XxTopKillerzZ <tomasfradecorral@hotmail.com>
2019-09-23 20:54:08 +0100

    Update install.sh

--- install-scripts/mariadb/install.sh
+++ install-scripts/mariadb/install.sh
@@ -8,10 +8,51 @@ echo
 echo -e "\e[32mCurrent Version: v0.1\e[39m"
 echo
 
+DatabaseCreation() {
+    	read -p "Do you want to create a database? " -n 1 -r
+	echo    # (optional) move to a new line
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		echo -e "\e[32mDatabase User Name:\e[39m"
+		while [[ $DATABASE_USER = "" ]]; do
+			read DATABASE_USER
+		done
+		
+		echo -e "\e[32mDatabase Name:\e[39m"
+		while [[ $DATABASE_NAME = "" ]]; do
+			read DATABASE_NAME
+		done
+    
+		echo -e "\e[32mDatabase Password: (Leave Blank for Random)\e[39m"
+		read DATABASE_PASSWORD
+		if [ -z "$DATABASE_PASSWORD" ]
+		then
+			echo -e "\e[32mGenerating Password\e[39m"
+			DATABASE_PASSWORD=$(date +%s | sha256sum | base64 | head -c 15 ; echo)
+			echo -e "\e[32msuccessfully Generated password...\e[39m"
+		fi
+    		create_file="/tmp/create-database.sh"
+		if [ -f "$create_file" ]
+		then
+			rm -r ./tmp/create-database.sh
+		fi
+		wget -P /tmp https://raw.githubusercontent.com/XxTopKillerzZ/WebGere/master/install-scripts/mariadb/create-database.sh
+		chmod a+x ./tmp/create-database.sh
+		./tmp/create-database.sh --host=localhost --database=$DATABASE_NAME --user=$DATABASE_USER --pass=$DATABASE_PASSWORD --rootpass=$ROOT_PASSWORD
+		rm -r ./tmp/create-database.sh
+  
+    
+	else
+		echo -e "\e[32mSkiping Database creation\e[39m"
+		echo -e "\e[32mMysql Root Password: $ROOT_PASSWORD\e[39m"
+	fi
+}
+
+
 if command -v mysql >/dev/null 2>&1 ; then
     echo -e "\e[32mMysql is already Installed...\e[39m"
-	echo "version: $(mysql --version)"
-	DatabaseCreation()
+	echo -e "\e[32mBersion: $(mysql --version)\e[39m"
+	DatabaseCreation
 else
     	echo -e "\e[32mInstalling Mysql...\e[39m"
 	
@@ -53,47 +94,6 @@ else
 	# setting the mysql bind-address to allow connections from everywhere
 	sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
 	
-	DatabaseCreation()
+	DatabaseCreation
 	
 fi
-
-
-DatabaseCreation() {
-    	read -p "Do you want to create a database? " -n 1 -r
-	echo    # (optional) move to a new line
-	if [[ $REPLY =~ ^[Yy]$ ]]
-	then
-		echo -e "\e[32mDatabase User Name:\e[39m"
-		while [[ $DATABASE_USER = "" ]]; do
-			read DATABASE_USER
-		done
-		
-		echo -e "\e[32mDatabase Name:\e[39m"
-		while [[ $DATABASE_NAME = "" ]]; do
-			read DATABASE_NAME
-		done
-    
-		echo -e "\e[32mDatabase Password: (Leave Blank for Random)\e[39m"
-		read DATABASE_PASSWORD
-		if [ -z "$DATABASE_PASSWORD" ]
-		then
-			echo -e "\e[32mGenerating Password\e[39m"
-			DATABASE_PASSWORD=$(date +%s | sha256sum | base64 | head -c 15 ; echo)
-			echo -e "\e[32msuccessfully Generated password...\e[39m"
-		fi
-    		create_file="/tmp/create-database.sh"
-		if [ -f "$create_file" ]
-		then
-			rm -r ./tmp/create-database.sh
-		fi
-		wget -P /tmp https://raw.githubusercontent.com/XxTopKillerzZ/WebGere/master/install-scripts/mariadb/create-database.sh
-		chmod a+x ./tmp/create-database.sh
-		./tmp/create-database.sh --host=localhost --database=$DATABASE_NAME --user=$DATABASE_USER --pass=$DATABASE_PASSWORD --rootpass=$ROOT_PASSWORD
-		rm -r ./tmp/create-database.sh
-  
-    
-	else
-		echo -e "\e[32mSkiping Database creation\e[39m"
-		echo -e "\e[32mMysql Root Password: $ROOT_PASSWORD\e[39m"
-	fi
-}
