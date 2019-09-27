f01b9f663da9027c6c064b6f4911d1056f7e88c8
XxTopKillerzZ <tomasfradecorral@hotmail.com>
2019-09-27 18:58:02 +0100

    Update manage.sh

--- install-scripts/fivem/manage.sh
+++ install-scripts/fivem/manage.sh
@@ -5,9 +5,9 @@
 #	Website: https://webgere.pt
 
 # Color Codes
-    NORMAL="\033[0;39m"
-    ROUGE="\033[1;31m"
-    VERT="\033[1;32m"
+    DEFAULT="\033[0;39m"
+    RED="\033[1;31m"
+    GREEN="\033[1;32m"
     ORANGE="\033[1;33m"
 	
 # Messages customs
@@ -38,23 +38,23 @@ case "$1" in
     start)
 	if ( running )
 	then
-	    echo -e "$ROUGE The [$SCREEN] server is already on !$NORMAL"
+	    echo -e "$RED The [$SCREEN] server is already on !$DEFAULT"
 	else
-		echo -e "$ROUGE Restarting mysql !$NORMAL"
+		echo -e "$RED Restarting mysql !$DEFAULT"
 		sudo service mysql restart
 		sleep 10
-        	echo -e "$ORANGE The [$SCREEN] server is going to start.$NORMAL"
+        	echo -e "$ORANGE The [$SCREEN] server is going to start.$DEFAULT"
 		screen -dm -S $SCREEN
 		sleep 2
 		screen -x $SCREEN -X stuff "cd "$FIVEM_PATH"/server-data && bash "$FIVEM_PATH"/server/run.sh +exec server.cfg 
 		"
-		echo -e "$ORANGE Session restart.$NORMAL"
+		echo -e "$ORANGE Session restart.$DEFAULT"
 		sleep 20
 		screen -x $SCREEN -X stuff "restart sessionmanager
 		"
-		echo -e "$VERT Session Ok ! $NORMAL"
+		echo -e "$GREEN Session Ok ! $DEFAULT"
 		sleep 5
-		echo -e "$VERT Server Ok ! $NORMAL"
+		echo -e "$GREEN Server Ok ! $DEFAULT"
 		time=$(date +"%m_%d_%Y")
 		echo -n "$time -- Server Started" >> $FIVEM_PATH/manage_log.txt
 	fi
@@ -63,12 +63,12 @@ case "$1" in
     stop)
 	if ( running )
 	then
-		echo -e "$VERT The server will stop in 10 seconds. $NORMAL"
+		echo -e "$GREEN The server will stop in 10 seconds. $DEFAULT"
         	screen -S $SCREEN -p 0 -X stuff "`printf "say $MSG_off\r"`"; sleep 10
 		screen -S $SCREEN -X quit
-        	echo -e "$ROUGE The [$SCREEN] server has been stopped.$NORMAL"
+        	echo -e "$RED The [$SCREEN] server has been stopped.$DEFAULT"
 		sleep 5
-    		echo -e "$VERT Cleaning the cache. $NORMAL"
+    		echo -e "$GREEN Cleaning the cache. $DEFAULT"
 		rm -R $FIVEM_PATH/server-data/cache/
 		echo -n "$time -- Server Stopped" >> $FIVEM_PATH/manage_log.txt
 	else
@@ -79,51 +79,51 @@ case "$1" in
 	restart)
 	if ( running )
 	then
-	    echo -e "$ROUGE The [$SCREEN] server us already running ! $NORMAL"
+	    echo -e "$RED The [$SCREEN] server us already running ! $DEFAULT"
 	else
-	    echo -e "$VERT The [$SCREEN] is stopped. $NORMAL"
+	    echo -e "$GREEN The [$SCREEN] is stopped. $DEFAULT"
 	fi
-	  echo -e "$ROUGE The server will restart... $NORMAL"
+	  echo -e "$RED The server will restart... $DEFAULT"
 		screen -S $SCREEN -p 0 -X stuff "`printf "say $MSG_180\r"`"; sleep 180
 		screen -S $SCREEN -p 0 -X stuff "`printf "say $MSG_60\r"`"; sleep 60
 		screen -S $SCREEN -p 0 -X stuff "`printf "say $MSG_30\r"`"; sleep 30
 		screen -S $SCREEN -X quit
-		echo -e "$VERT Server Closed $NORMAL"
+		echo -e "$GREEN Server Closed $DEFAULT"
 		rm -R $FIVEM_PATH/server-data/cache/
-		echo -e "$VERT Cleaning the cache. $NORMAL"
+		echo -e "$GREEN Cleaning the cache. $DEFAULT"
 		sleep 2
-		echo -e "$ORANGE Restart in progress ... $NORMAL"
-		echo -e "$ROUGE Restarting mysql !$NORMAL"
+		echo -e "$ORANGE Restart in progress ... $DEFAULT"
+		echo -e "$RED Restarting mysql !$DEFAULT"
 		sudo service mysql restart
 		sleep 10
-        	echo -e "$ORANGE [$SCREEN] is going to start.$NORMAL"
+        	echo -e "$ORANGE [$SCREEN] is going to start.$DEFAULT"
 		screen -dm -S $SCREEN
 		sleep 2
 		screen -x $SCREEN -X stuff "cd "$FIVEM_PATH"/server-data && bash "$FIVEM_PATH"/server/run.sh +exec server.cfg 
 		"
-		echo -e "$ORANGE Session Restart.$NORMAL"
+		echo -e "$ORANGE Session Restart.$DEFAULT"
 		sleep 20
 		screen -x $SCREEN -X stuff "restart sessionmanager
 		"
-		echo -e "$VERT [$SCREEN] is going to stop ! $NORMAL"
+		echo -e "$GREEN [$SCREEN] is going to stop ! $DEFAULT"
 		echo -n "$time -- Server Restarted" >> $FIVEM_PATH/manage_log.txt
 	;;	
     # -----------------[ Status ]---------------- #
 	status)
 	if ( running )
 	then
-	    echo -e "$VERT [$SCREEN] is running. $NORMAL"
+	    echo -e "$GREEN [$SCREEN] is running. $DEFAULT"
 	else
-	    echo -e "$ROUGE [$SCREEN]is closed. $NORMAL"
+	    echo -e "$RED [$SCREEN]is closed. $DEFAULT"
 	fi
 	;;
     # -----------------[ Screen ]---------------- #
     screen)
-        echo -e "$VERT Server Console [$SCREEN]. $NORMAL"
+        echo -e "$GREEN Server Console [$SCREEN]. $DEFAULT"
         screen -R $SCREEN
     ;;
 	*)
-    echo -e "$ORANGE Options :$NORMAL ./manage.sh {start|stop|status|screen|restart}"
+    echo -e "$ORANGE Options :$DEFAULT ./manage.sh {start|stop|status|screen|restart}"
     exit 1
     ;;
 esac
